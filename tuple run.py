1f7d9cfc11905a38e6c5d0f742147edc42d05e27
Zebra882 <sultanofnepal@gmail.com>
2024-11-18 18:26:06 +0545

    Add files via upload

---
+++ tuple run.py
@@ -0,0 +1,19 @@
+tuplex=("hassan" ,False,0.3,69)
+print(tuplex)
+#create tuple with integer value
+
+tuplex=(1,2,3,4,5,6,7,8,9,10)
+print(tuplex)
+
+tuple1=tuplex +(69,)
+print(tuple1)
+
+tuple2=(17,35,27,38,421,387,46,542,56,402,564,23,142,34)
+_slice=tuple2[3:9]
+print(_slice)
+
+_slice=tuple2[:6]
+print(_slice)
+
+tuple3=(1,2,1,3,4,5,6,5,7,8,1,2,6,4,9)
+print(tuple3.count(1))
